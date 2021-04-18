c9847822d3f9f8afc411095ee63d5aaaf4fea9ec
iadaba <ibukunadaba@gmail.com>
2021-04-18 16:48:03 -0400

    Update names of file PATH

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/iadaba/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
 apt-get update
